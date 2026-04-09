8590b757877bdd4dca15f5f0596d1807bae07e71
tidum41 <mudit14.mahajan@gmail.com>
2026-04-08 23:48:30 -0700

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight, Home, Settings, Check } from 'lucide-react';
 import { motion, AnimatePresence } from 'motion/react';
 import { Switch } from './components/ui/switch';
